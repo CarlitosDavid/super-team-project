58f9c31785f0dbeb4ce8becaba619396c90e1f6f
Carl Contreras <contrerascarl09@gmail.com>
2021-11-30 18:55:00 -0600

    on 6.2.5 modules

---
+++ assets/js/homepage.js
@@ -0,0 +1,47 @@
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var getUserRepos = function(user) { 
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+    
+};
+
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value form input element
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+response.json().then(function(data) {
+    displayRepos(data, user);
+});
